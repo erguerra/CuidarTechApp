6f4d317e77dac07b809dc06ac38e639fb6904732
Eduardo Guerra <erguerra.dev@gmail.com>
2024-12-19 16:38:28 -0300

    remove unused imports

--- composeApp/src/commonMain/kotlin/br/com/cuidartech/app/ui/subjects/HomeContent.kt
+++ composeApp/src/commonMain/kotlin/br/com/cuidartech/app/ui/subjects/HomeContent.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
@@ -22,10 +21,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
-import br.com.cuidartech.app.ui.components.Header
 import br.com.cuidartech.app.ui.model.SubjectUIModel
 import cuidartechapp.composeapp.generated.resources.Res
 import cuidartechapp.composeapp.generated.resources.background_pattern
